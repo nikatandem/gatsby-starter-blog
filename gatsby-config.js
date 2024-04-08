7287a2630752aa37a08081393fdd76776fcf5d53
nikatandem <nikatandem@gmail.com>
2024-04-08 13:19:57 +0200

    configuracion

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Blog de Nika`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Veronica Huertas`,
+      summary: `Practicando con gatsby y netlify`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Un blog de prueba`,
+    siteUrl: `https://github.com/nikatandem`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
